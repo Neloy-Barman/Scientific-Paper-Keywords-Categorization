ed41448840e8ef68d8dabe7fb57f000bb828ee54
Neloy Barman <neloycareer018@gmail.com>
2023-12-31 18:07:35 +0600

    scraped more details

--- scraper_files/details_scraper_2.py
+++ scraper_files/details_scraper_2.py
@@ -19,7 +19,7 @@ def main():
 
     driver = webdriver.Chrome()
 
-    for index in tqdm(range(4081,len(urls))):
+    for index in tqdm(range(13018,len(urls))):
         
         url = urls[index]
 
